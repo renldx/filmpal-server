97dc8ff0f538d72fa4813ed629137fb5775ac9c8
renldx <renldx@gmail.com>
2025-01-28 14:43:30 -0500

    Add generated project structure

---
+++ src/main/java/com/renldx/filmpal/FilmpalApplication.java
@@ -0,0 +1,13 @@
+package com.renldx.filmpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmpalApplication.class, args);
+	}
+
+}
