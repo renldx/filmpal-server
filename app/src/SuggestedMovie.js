84344ac5afc123748d39a1521634a16b287f43da
renldx <renldx@gmail.com>
2025-02-01 20:13:38 -0500

    Minor movie card UI refactor

--- app/src/SuggestedMovie.js
+++ app/src/SuggestedMovie.js
@@ -1,24 +1,25 @@
+import {Link} from "react-router-dom";
 import {Card, CardBody, CardSubtitle, CardText, CardTitle} from "reactstrap";
 
 const SuggestedMovie = ({movie}) => {
-    const releaseYear = (date) => {
-        return new Date(date).getFullYear();
-    }
+    const releaseYear = (date) => new Date(date).getFullYear();
 
     return (
-        <Card style={{minWidth: '16rem', maxWidth: '32rem', margin: '0.5rem'}}>
-            <img alt="Sample" src="https://picsum.photos/300/200"/>
-            <CardBody>
-                <CardTitle tag="h5">
-                    {movie.title}
-                </CardTitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                    {releaseYear(movie.release)}
-                </CardSubtitle>
-                <CardText>
-                </CardText>
-            </CardBody>
-        </Card>
+        <Link to="#" style={{textDecoration: 'none'}}>
+            <Card style={{width: '24rem', margin: '0.5rem'}}>
+                <img alt="Sample" src="https://picsum.photos/300/200"/>
+                <CardBody>
+                    <CardTitle tag="h5">
+                        {movie.title}
+                    </CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                        {releaseYear(movie.release)}
+                    </CardSubtitle>
+                    <CardText>
+                    </CardText>
+                </CardBody>
+            </Card>
+        </Link>
     );
 }
 
