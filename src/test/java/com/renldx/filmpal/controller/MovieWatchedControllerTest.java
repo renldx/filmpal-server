34bbaf9f60b5c1d68c46f65447835d1b413c10c9
renldx <renldx@gmail.com>
2025-02-07 10:21:32 -0500

    Partial work on watched movies controller unit tests

--- src/test/java/com/renldx/filmpal/controller/MovieWatchedControllerTest.java
+++ src/test/java/com/renldx/filmpal/controller/MovieWatchedControllerTest.java
@@ -1,40 +1,50 @@
 package com.renldx.filmpal.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.renldx.filmpal.entity.MovieDto;
+import com.renldx.filmpal.service.MovieWatchedService;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(MovieWatchedController.class)
 class MovieWatchedControllerTest {
+
+    @MockitoBean
+    private MovieWatchedService movieWatchedService;
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Nested
     class getWatchedMoviesTest {
 
         @Test
         void getWatchedMovies_ReturnsWatchedMovies() throws Exception {
-            var result = mockMvc.perform(get("/api/watched/movies/"))
+            var mockMovie = new MovieDto("TestMovie", new Date());
+            var mockWatchedMovies = new ArrayList<MovieDto>();
+            mockWatchedMovies.add(mockMovie);
+
+            when(movieWatchedService.getMovies()).thenReturn(mockWatchedMovies);
+
+            var result = mockMvc.perform(get("/api/watched/movies"))
                     .andDo(print())
                     .andExpect(status().isOk())
                     .andReturn();
 
             var json = result.getResponse().getContentAsString();
 
-            assert (!json.isBlank()); // TODO: Add seed data
+            assert (!json.isBlank());
         }
 
     }
